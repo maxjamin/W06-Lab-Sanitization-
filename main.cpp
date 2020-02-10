05f7d76415edffa1a1539ae9d602dc0fbdfac637
ghostMan <maxjamin@gmail.com>
2020-02-10 11:06:39 -0700

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/***********************************************************************
+* Program:
+*    Lab 06, Sanitization
+*    Brother Helfrich, CS 370
+* Author:
+*    John Miller
+*    Bretton Steiner
+*	 Ben Smith
+*    Tyler Starr
+*
+* Summary:
+*    Enter a brief description of your program here!
+ ************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***********************************************************************
+* Function:
+*    main()
+* Inputs: none
+* Summary:
+*    Driver function for the program.
+ ************************************************************************/
+int main()
+{
+
+}
