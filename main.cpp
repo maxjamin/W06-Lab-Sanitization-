4a6d7c08c09e9073fa719038238afe7c43ede4cc
ghostMan <maxjamin@gmail.com>
2020-02-10 11:42:49 -0700

    test case function 1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <string>
 /***********************************************************************
 * Program:
 *    Lab 06, Sanitization
@@ -12,17 +14,52 @@
 *    Enter a brief description of your program here!
  ************************************************************************/
 
+/***********************************************************************
+* Function:
+* 	queryGeneration()
+* Inputs: 
+* Summary:
+*************************************************************************/
+int queryGeneration(std::string *output, std::string username, std::string password)
+{
+	if(username == "" || password == "")
+		return 1;
 
+	*output = "SELECT authenticate FROM passwordList WHERE name='" + 
+	username  + "' and passwd='" + password + "';";
+	return 0;
+}
 
+/***********************************************************************
+* Function:
+*    main()
+* Inputs: string pointer input, string prompt
+* Summary:
+*    ask for the user input
+ ************************************************************************/
+ void getUserInput(std::string *input, std::string promptString)
+ {
+ 	printf(" %s",promptString.c_str());
+ 	getline(std::cin, *input);
+ }
 
+/***********************************************************************
+* Function:
+* 	validInputTest()
+* Inputs: 
+* Summary:
+*************************************************************************/
+int validInputTest()
+{
+	std::string username;
+	std::string password;
+	std::string sqlQuery;
 
-
-
-
-
-
-
-
+	/*Test case 1  Example valid username and password*/
+	username = "bill"; password="test123";
+	queryGeneration(&sqlQuery, username, password);
+	std::cout << "sql Query " << sqlQuery << "\n";
+}
 
 /***********************************************************************
 * Function:
@@ -33,5 +70,7 @@
  ************************************************************************/
 int main()
 {
+	validInputTest();
 
-}
+
+}	
